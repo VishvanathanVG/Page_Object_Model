5dbdadae381bc30e9620ceb857d182036157b793
Mouneesh <VishvanathanVG>
2019-11-09 22:54:28 +0530

    first commit -Page object model

---
+++ PageObjectModel/src/main/java/org/testleaf/qa/pages/MyHomePage.java
@@ -0,0 +1,14 @@
+package org.testleaf.qa.pages;
+
+import org.testleaf.qa.baseAPI.ProjectSpecificMethods;
+
+public class MyHomePage extends ProjectSpecificMethods{
+
+	public MyLeadPage clickOnLead() {
+		
+		driver.findElementByLinkText("Leads").click();
+		return new MyLeadPage();
+	}
+	
+	
+}
---
+++ PageObjectModel/src/main/java/org/testleaf/qa/pages/MyLeadPage.java
@@ -0,0 +1,27 @@
+package org.testleaf.qa.pages;
+
+import org.testleaf.qa.baseAPI.ProjectSpecificMethods;
+
+public class MyLeadPage extends ProjectSpecificMethods{
+
+	public CreateLeadPage clickOnCreateLead() {
+		
+		driver.findElementByLinkText("Create Lead").click();
+		return new CreateLeadPage();
+		
+	}
+	
+	public FindLeadPage clickOnFindLeads() {
+		
+		driver.findElementByXPath("//a[contains(text(),'Find Leads')]").click();
+		
+		return new FindLeadPage();
+	}
+	
+	public MergeLeadPage clickOnMergeLead() {
+		
+		driver.findElementByLinkText("Merge Leads").click();
+		return new MergeLeadPage();
+	}
+	
+}
